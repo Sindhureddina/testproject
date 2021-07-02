13fa6f04b5c39c4cf9d8a955849127cee54a5d13
Sindhu <Sindhu@LAPTOP-HJQLU3DH>
2021-07-02 14:31:16 +0530

    uploaded files

---
+++ HelloWorldSpringBoot/src/main/java/com/infotech/app/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.infotech.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HelloWorldController {
+	
+	@RequestMapping("/hello")
+	
+	public String hello(){
+		return "Hello World!!";
+		
+	}
+
+}
